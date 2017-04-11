70d0e6998b3c305ec8fd290101339624704cdbcd
Jerems91 <jerem92@free.fr>
2017-04-11 09:31:48 +0200

    JEDE 11/04/2017
    Création application Angular 2 intégrée dans un projet ASP.NET Core MVC en utilisant le template Angular fourni dans la librairie Microsoft.AspNetCore.SpaTemplates

---
+++ ASPNETCoreSPAv2/Client/app/browser-app.module.ts
@@ -0,0 +1,48 @@
+﻿import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { APP_BASE_HREF } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { SignalRModule, SignalRConfiguration } from 'ng2-signalr';
+
+import { ORIGIN_URL } from './shared/constants/baseurl.constants';
+import { AppModule } from './app.module';
+import { AppComponent } from './app.component';
+
+export function createConfig(): SignalRConfiguration {
+    const signalRConfig = new SignalRConfiguration();
+
+    signalRConfig.hubName = 'Ng2SignalRHub';
+    signalRConfig.qs = { user: 'donald' };
+    signalRConfig.url = 'http://ng2-signalr-backend.azurewebsites.net/';
+    signalRConfig.logging = true;
+
+    return signalRConfig;
+}
+
+export function getOriginUrl() {
+  return window.location.origin;
+}
+
+@NgModule({
+    bootstrap: [AppComponent],
+    imports: [
+        BrowserModule.withServerTransition({
+            appId: 'my-app-id' // make sure this matches with your Server NgModule
+        }),
+        BrowserAnimationsModule,
+        // Our Common AppModule
+        AppModule,
+
+        SignalRModule.forRoot(createConfig)
+    ],
+    providers: [
+        {
+            // We need this for our Http calls since they'll be using APP_BASE_HREF (since the Server requires Absolute URLs)
+            provide: ORIGIN_URL,
+            useFactory: (getOriginUrl)
+        }
+    ]
+})
+export class BrowserAppModule {
+}
---
+++ ASPNETCoreSPAv2/Startup.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.IO;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Antiforgery;
+
+using Microsoft.AspNetCore.SpaServices.Webpack;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+
+using Microsoft.AspNetCore.NodeServices;
+using AspCoreServer.Data;
+
+namespace AspCoreServer
+{
+    public class Startup
+    {
+
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add framework services.
+            services.AddMvc();
+            services.AddNodeServices();
+
+            var connectionStringBuilder = new Microsoft.Data.Sqlite.SqliteConnectionStringBuilder { DataSource = "spa.db" };
+            var connectionString = connectionStringBuilder.ToString();
+
+            services.AddDbContext<SpaDbContext>(options =>
+                options.UseSqlite(connectionString));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, SpaDbContext context)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseWebpackDevMiddleware(new WebpackDevMiddlewareOptions {
+                    HotModuleReplacement = true
+                });
+
+                DbInitializer.Initialize(context);
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+
+                routes.MapSpaFallbackRoute(
+                    name: "spa-fallback",
+                    defaults: new { controller = "Home", action = "Index" });
+            });
+        }
+    }
+}
